d270db65c52a48c8a9880e09e7752acdd01e2570
mthzg <mathias.gerday@gmail.com>
2024-02-02 11:56:57 +0100

    version fonction function session id

--- scriptauth.js
+++ scriptauth.js
@@ -1,48 +1,39 @@
-async function test() {
-    const login = document.querySelector(".login");
-    login.addEventListener('click', async () => {
-      const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWI1MzgyMmUwNzlhMjA3OWEyNTU3YmY4YzE2YjFhOCIsInN1YiI6IjY1YjRmN2U3NThlZmQzMDE2M2NhM2E1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8jpbHJlS1LVUQ7D2ulav92LOTi88tzJGZZ7RlcwrA9c'
-        }
-      };
-      try {
-        const response = await fetch('https://api.themoviedb.org/3/authentication/token/new', options);
-        const data = await response.json();
-        let request_token = data.request_token;
-        console.log(request_token);
-        let bool = data.success;
-        if (bool === true) {
-          window.open("https://www.themoviedb.org/authenticate/" + request_token + "?redirect_to=http://127.0.0.1:5500/auth.html");
-          return request_token;
-        }
-      } catch (err) {
-        console.error(err);
+function recupToken(){
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjhhOTgxMGFlYjRlMmM1ZWZjNGEzZmQyMTc0NDRiYyIsInN1YiI6IjY1Yjc1YmJlZDU1YzNkMDE3Y2ZhYzBiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0XfH-ZZ0XcDH-KXR7ZINt7KhzuICXcbvlnaNrzc0aEM'
+    }};
+  fetch('https://api.themoviedb.org/3/authentication/token/new', options)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+    let token = data.request_token;
+    console.log(token);
+   const auth = window.open("https://www.themoviedb.org/authenticate/" + token + "?redirect_to=http://127.0.0.1:5500/auth.html");
+   setTimeout(() => {
+     if (auth.closed === true || auth.closed === false) {
+       console.log("closed")
+       console.log(token,"test")
+       const options2 = {
+         method: 'POST',
+         headers: {
+           accept: 'application/json',
+           'content-type': 'application/json',
+           Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjhhOTgxMGFlYjRlMmM1ZWZjNGEzZmQyMTc0NDRiYyIsInN1YiI6IjY1Yjc1YmJlZDU1YzNkMDE3Y2ZhYzBiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0XfH-ZZ0XcDH-KXR7ZINt7KhzuICXcbvlnaNrzc0aEM"
+          },
+          body: JSON.stringify({request_token: token}),
+        };
+        fetch('http://api.themoviedb.org/3/authentication/session/new', options2)
+        .then(respons => respons.json())
+        .then((data) => {
+          console.log(data)})
       }
-    });
-  }
-  
-  async function createSession(request_token) {
-    const options = {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-          'content-type': 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOWI1MzgyMmUwNzlhMjA3OWEyNTU3YmY4YzE2YjFhOCIsInN1YiI6IjY1YjRmN2U3NThlZmQzMDE2M2NhM2E1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8jpbHJlS1LVUQ7D2ulav92LOTi88tzJGZZ7RlcwrA9c'
-        },
-        body: JSON.stringify({request_token: '6bc047b88f669d1fb86574f06381005d93d3517a'})
-      };
-      
-      fetch('https://api.themoviedb.org/3/authentication/session/new', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    }
-  
-  test();
-  setInterval(async () => {
-    const request_token = await test();
-    await createSession(request_token);
-  }, 5000);
+    }, 5000);
+  })
+}
+
+login = document.querySelector('.login').addEventListener('click', function (e) {
+  recupToken();
+});
