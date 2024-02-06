c4b43a5e62ae829f14c8413bf20067514372bf53
erwann quin <erwann.quin@supinfo.com>
2024-02-06 21:59:50 +0100

    ajout infinite scroll recherche.html

--- scriptRecherche.js
+++ scriptRecherche.js
@@ -1,30 +1,57 @@
-function rechercheAPI()
+function rechercheAPI(pageTrending,startEndVariable)
 {
-    varRecherche = barreRecherche.value
-    fetch("https://api.themoviedb.org/3/search/movie?query="+varRecherche+"&api_key=db8a9810aeb4e2c5efc4a3fd217444bc")
+    varRecherche = document.querySelector('.barre-recherche').value
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjhhOTgxMGFlYjRlMmM1ZWZjNGEzZmQyMTc0NDRiYyIsInN1YiI6IjY1Yjc1YmJlZDU1YzNkMDE3Y2ZhYzBiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0XfH-ZZ0XcDH-KXR7ZINt7KhzuICXcbvlnaNrzc0aEM'
+        }
+      };
+    fetch("https://api.themoviedb.org/3/search/movie?query="+varRecherche+"&page="+pageTrending, options)
         .then((response) => response.json())
         .then((data) => {
-            divResultats = document.querySelector('.resultats-recherche')
-            divResultats.innerHTML = ""
-            for (let i = 0; i < 20; i++){
-                affichageFilms(data.results[i],i);
+            for (let i = startEndVariable; i < startEndVariable+20; i++){
+                filmActuel = i-startEndVariable
+                affichageFilms(data.results[filmActuel]);
             }
         });
     }
     
     
-    function affichageFilms(film)
-    {
-        //console.log(film)
-        divResultats = document.querySelector('.resultats-recherche')
-        let voteInt = (Math.round(film.vote_average*10))/10
-        divResultats.innerHTML = divResultats.innerHTML + `<a href="film.html?id=`+film.id+`" class="resultat a`+film.id+`"><img src="https://image.tmdb.org/t/p/original`+film.poster_path+`"><h2>`+film.title+`</h2><p>`+voteInt+`</p></a>`
-    }
+function viderPage() {
+    divResultats = document.querySelector('.resultats-recherche')
+    divResultats.innerHTML = ""
+}
+
+function affichageFilms(film)
+{
+    divResultats = document.querySelector('.resultats-recherche')
+    let voteInt = (Math.round(film.vote_average*10))/10
+    divResultats.innerHTML = divResultats.innerHTML + `<a href="film.html?id=`+film.id+`" class="resultat a`+film.id+`"><img src="https://image.tmdb.org/t/p/original`+film.poster_path+`"><h2>`+film.title+`</h2><p>`+voteInt+`</p></a>`
+}
+
+function actualisationResultats() {
+    viderPage()
+    rechercheAPI(pageTrending,startEndVariable)
+    let mainDiv = document.querySelector(".grid-general");
+    mainDiv.onscroll = (e) => {
+        let positionDuClient = document.documentElement.scrollTop+document.documentElement.clientHeight;
+        let positionMaxPage = document.documentElement.scrollHeight;
+        if (positionDuClient >= positionMaxPage) {
+          startEndVariable += 20;
+          pageTrending += 1;
+          rechercheAPI(pageTrending,startEndVariable)
+        }
+      }    
+}
     
-    
-    barreRecherche = document.querySelector('.barre-recherche');
-    barreRecherche.addEventListener('input', rechercheAPI)
-    
+let startEndVariable = 0;
+let pageTrending = 1;
+barreRecherche = document.querySelector('.barre-recherche');
+barreRecherche.addEventListener('input', actualisationResultats);
+
+
     
     //                    const expireDate = new Date()
     //                    expireDate.setTime(expireDate.getTime() + 30000)
